24afd524526c8cbd438e19707bf89c750a794000
seanpgallivan <seanpgallivan@hotmail.com>
2019-12-19 09:02:32 -0800

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,22 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
+
+    def name_with_country
+        "#{self.name}, #{self.country}"
+    end
+
+    def featured
+        self.posts.order(:likes).last
+    end
+
+    def most_recent
+        self.posts.order('created_at desc').limit(5)
+    end
+
+    def average_age
+        self.bloggers ? (self.bloggers.sum(:age).to_f / self.bloggers.count) : "N/A"
+    end
+
 
 end
